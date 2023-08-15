a5a1372f62c8d7192e631f7aede29b4ffceb431d
JoyPJ11 <jumajoy320@gmail.com>
2023-08-15 07:17:49 -0700

    printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - Aprogram that prints using puts
  * Return: 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <sydio>
+/**
+ * main -a program that displays using printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
